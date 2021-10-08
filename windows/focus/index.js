9ae140d94921e62d4b5221e4e07c46684f6640d0
Alexander Glushchenko <pistch@yandex-team.ru>
2021-10-08 10:00:32 +0300

    automanage focus window

--- windows/focus/index.js
+++ windows/focus/index.js
@@ -1,5 +1,7 @@
 const path = require('path');
-const {BrowserWindow, globalShortcut} = require('electron');
+const {BrowserWindow} = require('electron');
+
+const {hideWindow} = require('../utils');
 
 module.exports = function(store) {
   const win = new BrowserWindow({
@@ -12,29 +14,25 @@ module.exports = function(store) {
     focusable: false,
   });
 
-  function sendStateToWindow() {
+  function showWindow() {
+    win.show();
+    win.setAlwaysOnTop(true, 'status');
+  }
+
+  function applyStateUpdate() {
     const state = store.store.getState();
 
-    // if (state.focusedItem && !win.isVisible()) {
-    //   app.show();
-    //   win.show();
-    //   win.setAlwaysOnTop(true, 'status');
-    // } else if (win.isVisible()) {
-    //   win.hide();
-    // }
+    if (!state.focusedItem && win.isVisible()) {
+      hideWindow(win);
+    }
+
+    if (state.focusedItem && !win.isVisible()) {
+      showWindow();
+    }
 
     win.webContents.send('state-update', store.store.getState());
   }
 
   win.loadFile(path.resolve(__dirname, 'static/index.html'));
-
-  globalShortcut.register('Alt+Shift+F', () => {
-    if (!win.isVisible()) {
-      win.show();
-      win.setAlwaysOnTop(true, 'status');
-    } else {
-      win.hide();
-    }
-  });
-  store.store.subscribe(sendStateToWindow);
+  store.store.subscribe(applyStateUpdate);
 };
